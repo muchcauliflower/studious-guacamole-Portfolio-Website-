204cba4cfac5c2480b32abfe4b61f028c1eee58b
Kale <166086932+muchcauliflower@users.noreply.github.com>
2025-11-24 15:55:40 +0800

    Imrpoved UI, added Relevant Skills Sections
    Improved UI, change nav buttons to animate. Added Relevant Skill Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,15 @@
-import TypeWriterEffect from "./components/TypeWriterEffect";
-import RotatingText from "./components/RotatingText/RotatingText";
-import RotatingTextFunction from "./components/RotatingText/RotatingTextFunction";
-import StarBorder from "./components/StarBorder/StarBorder";
+import HomePageMainComponent from "./components/HomePageMain/HomePageMainComponent";
+import TopBar from "./components/TopBar/TopBar";
+import { SkillSetComponent } from "./components/HomePageSkillSetSection/SkillSetComponent";
 
 function App() {
   return (
     <div className="homepage">
-      {/* main section */}
       <div>
-        <header className="topbar">
-          <div className="col-sm-8 p-1">
-            <h1 className="IndexFont">
-              Much<span className="highlight-green">Cauliflower</span>
-            </h1>
-          </div>
-          <div className="container text-center">
-            <div className="row align-items-start">
-              <div className="col">
-                <h3>About</h3>
-              </div>
-              <div className="col">
-                <h3>Projects</h3>
-              </div>
-              <div className="col">
-                <h3>Contact</h3>
-              </div>
-            </div>
-          </div>
-        </header>
+        <TopBar />
+        <HomePageMainComponent />
+        <SkillSetComponent />
 
-        <main>
-          <h1 className="title">
-            Hello,
-            <br /> I am{" "}
-            <span className="highlight-animated-green">Mikhail</span>,
-            <br /> Aspiring{""}
-            <br />
-            <RotatingTextFunction />
-          </h1>
-          <p className="subtitle">
-            Currently Located in Philippines. Driven by{" "}
-            <span className="paragraph-highlight">Back-End development</span>,
-            and currently{" "}
-            <span className="paragraph-highlight">
-              leaning into Front-End development
-            </span>{" "}
-            to round out my Full-Stack abilities!
-          </p>
-          <StarBorder
-            as="button"
-            className="custom-class"
-            color="rgb(29, 255, 153)"
-            speed="2s"
-          >
-            Let's get in touch!
-          </StarBorder>
-        </main>
-
-        {/* skillset section */}
         {/* <div className="skillset-section"></div> */}
       </div>
     </div>
