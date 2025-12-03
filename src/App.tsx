5089aba6be950c342e97767fdc042c2e2445618c
Kale <166086932+muchcauliflower@users.noreply.github.com>
2025-12-04 00:28:39 +0800

    Added Languages And tools Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import HomePageMainComponent from "./components/HomePageMain/HomePageMainComponent";
-import TopBar from "./components/TopBar/TopBar";
-import { SkillSetComponent } from "./components/HomePageSkillSetSection/SkillSetComponent";
+import HomePageMainComponent from "./components/Sections/HomePageMainSection/HomePageMainComponent";
+import { SkillSetComponent } from "./components/Sections/HomePageSkillSetSection/SkillSetComponent";
+import TopBar from "./components/HardCoded/TopBar/TopBar";
+import Aurora from "./components/ReactBitsLibrary/Aurora/Aurora";
+import { LoopingIcons } from "./components/HardCoded/LoopingText/LoopingIcons";
+import LogoDisplay from "./components/HardCoded/LogoDisplay/LogoDisplay";
 
 function App() {
   return (
     <div className="homepage">
-      <div>
-        <TopBar />
-        <HomePageMainComponent />
-        <SkillSetComponent />
-
-        {/* <div className="skillset-section"></div> */}
+      <TopBar />
+      <HomePageMainComponent />
+      <SkillSetComponent />
+      <div className="LanguagesAndToolsSection">
+        <Aurora
+          colorStops={["#028f62", "#028f62", "#028f62"]}
+          blend={0.5}
+          amplitude={2.0}
+          speed={0.5}
+        />
+      </div>
+      <div className="LanguagesAndToolsSectionContainer">
+        <div className="LanguagesAndToolsSectionContents">
+          <LogoDisplay />
+        </div>
       </div>
     </div>
   );
